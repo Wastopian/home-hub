ac5e5fbd20cd46d3a6b7d84132c17d04721ea527
Wastopian <wastopia@gmail.com>
2025-06-10 21:06:37 -0500

    Improve test and bump build memory

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home Hub heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByRole('heading', { name: /Home Hub/i });
+  expect(heading).toBeInTheDocument();
 });
